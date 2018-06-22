b06d8f91cea692ed7569fc766e5e78516dce207d
reza <rsefzaab@yahoo.com>
2018-06-23 03:03:00 +0430

    Add OrderController and  etc

--- SportsStore/Migrations/20180622121044_Orders.Designer.cs
+++ SportsStore/Migrations/20180622121044_Orders.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180622121044_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -62,8 +63,6 @@ namespace SportsStore.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired();
-
-                    b.Property<bool>("Shipped");
 
                     b.Property<string>("State")
                         .IsRequired();
